3b9c2a09fc340f79c2d4012be036a8fa8739f1b5
RajiSDET <rajalakshmisdet@gmail.com>
2024-10-22 00:56:30 +0530

    Adding one more user story and changes in testng.xml

--- src/main/java/Sprint_01/RAJI_S1_012_CreateNewTask_S6_61.java
+++ src/main/java/Sprint_01/RAJI_S1_012_CreateNewTask_S6_61.java
@@ -3,14 +3,18 @@ package Sprint_01;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class RAJI_S1_012_CreateNewTask_S6_61 {
-	
-public static void main(String[] args) throws InterruptedException {
+public class RAJI_S1_012_CreateNewTask_S6_61 extends BaseClass {
+
+ @Test
+public  void createNewTask() throws InterruptedException {
 		
-		ChromeOptions options = new ChromeOptions();
+		/*ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
 		
 		ChromeDriver driver = new ChromeDriver(options);
@@ -22,7 +26,7 @@ public static void main(String[] args) throws InterruptedException {
 	//Step 1: Login to Sales-force Application
 		driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
 		driver.findElement(By.id("password")).sendKeys("Leaf$321");
-		driver.findElement(By.id("Login")).click();
+		driver.findElement(By.id("Login")).click();*/
 	
 	
 	//Step 2: Click on toggle menu button from the left corner
@@ -32,13 +36,37 @@ public static void main(String[] args) throws InterruptedException {
 				driver.findElement(By.xpath("//button[text()='View All']")).click();
 				
 				
-	//Step 4. Click on Content tab 
+	//Step 4. Click on Content tab 				
+				driver.findElement(By.xpath("//p[text()='Content']")).click();
+				
+				Thread.sleep(1000);
+				
 	//Step 5. Click View All from Today's Task
-	//Step 6. Click on Show one more Action and click New Task
-	//Step 7. Select a Account Name in Assigned to field 
+				driver.findElement(By.xpath("(//span[@class=\"viewAllLabel\"])[3]")).click();
+				Thread.sleep(1000);
+	//Step 6. Click on Show two more Action and click New Task
+				driver.findElement(By.xpath("//div[@class='leftRegion']//a[@title='Show 2 more actions']")).click();
+				driver.findElement(By.xpath("//a[@title='New Task']")).click();
+	//Step 7. Select a Account Name in Assigned to field and set Status as In Progress
+				driver.findElement(By.xpath("//input[@title='Search People']")).sendKeys(Keys.TAB,Keys.TAB,Keys.ENTER);
+				driver.findElement(By.xpath("//div[@class='select-options']//a[@title='In Progress']")).click();
 	//Step 8. Select a subject as Email
-	//Step 9. Set Priority as High and Status as In Progress
-	//Step 10. Click on the image of Name field, click on Contacts and select Contact
-	//Step 11. Click on the image of Related To field, click on Product and Select Product
-
+				driver.findElement(By.xpath("//div[@data-target-selection-name='sfdc:RecordField.Task.Subject']//input")).sendKeys("Email",Keys.TAB,Keys.ENTER);
+	//Step 9. Click on the image of Name field, click on Contacts and select Contact
+				driver.findElement(By.xpath("//a[@title='Contacts']")).click();
+				driver.findElement(By.xpath("//input[@title='Search Contacts']")).click();
+				driver.findElement(By.xpath("(//div[@class='listContent'])[2]//li")).click();
+	//Step 10. Click on the image of Related To field, click on Product and Select Product
+				driver.findElement(By.xpath("//img[@alt='Accounts']")).click();
+				driver.findElement(By.xpath("//span[@title='Products']")).click();
+				driver.findElement(By.xpath("//input[@title='Search Products']")).click();
+				driver.findElement(By.xpath("(//div[@class=\"listContent\"])[3]//li")).click();
+	//Step 11. Set Priority as High and Click on Save
+				driver.findElement(By.xpath("//div[@data-target-selection-name='sfdc:RecordField.Task.Priority']//a")).click();
+				driver.findElement(By.xpath("//a[@title='High']")).click();
+				driver.findElement(By.xpath("//span[text()='Save']")).click();
+				String ActualMsg = driver.findElement(By.xpath("//div[@role=\"alert\"]//span[@data-aura-class=\"forceActionsText\"]")).getText();
+            System.out.println(ActualMsg);
+            String ExpectedMsg ="Task \"Email\" was created.";
+            Assert.assertEquals(ActualMsg, ExpectedMsg);
 }}
