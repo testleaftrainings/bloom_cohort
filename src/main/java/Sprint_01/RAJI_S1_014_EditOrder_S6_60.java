de89886eb7c98ae0a17655236954266034b69b84
RajiSDET <rajalakshmisdet@gmail.com>
2024-10-22 23:58:03 +0530

    RAJI_Commiting changes for the 2 new userstories

--- src/main/java/Sprint_01/RAJI_S1_014_EditOrder_S6_60.java
+++ src/main/java/Sprint_01/RAJI_S1_014_EditOrder_S6_60.java
@@ -3,6 +3,8 @@ package Sprint_01;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -30,12 +32,22 @@ public static void main(String[] args) throws InterruptedException {
 
 	//Step 3: Click view All from App Launcher
 				driver.findElement(By.xpath("//button[text()='View All']")).click();
-				
-				
+		
 	//Step 4. Click on Service Console from App Launcher
+				driver.findElement(By.xpath("//p[text()='Service Console']")).click();
 	//Step 5. Click on the drop down and select Orders
+				driver.findElement(By.xpath("//button[@title='Show Navigation Menu']")).click();
+				driver.findElement(By.xpath("//a[@data-itemid='Order']")).click();
 	//Step 6. Click drop down near Recently Viewed and Select All Orders
+				driver.findElement(By.xpath("//button[@title='Select a List View: Orders']")).click();
+				driver.findElement(By.xpath("//div[@data-aura-class=\"uiAbstractList forceVirtualAutocompleteMenuList\"]//span[text()='All Orders']")).click();
 	//Step 7. Select first result, click the dropdown of the result and click on Edit
+				driver.findElement(By.xpath("(//table[@aria-label='All Orders']//td[2])[1]")).click();
+				//driver.findElement(By.xpath("//table[@aria-label='All Orders']//td//span[text()='Show Actions']")).click();
+				WebElement element = driver.findElement(By.xpath("//table[@aria-label='All Orders']//td//span[text()='Show Actions']"));
+				((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+
+				driver.findElement(By.xpath("//a[@title='Edit']")).click();
 	//Step 8. Click on the close button form the Account Name Field and Select Account name as Testing001
 	//Step 9. Click on the close button form theContract Number field and select Contract number as 00000104
 	//Step 10.Select Order Start Date as next month 10th date
